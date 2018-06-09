afd412e806383ab52b675862c93e7ca4d8b0f567
Brandon Langdon <bfglangdon@gmail.com>
2018-06-09 10:03:52 -0700

    General update
    Outlined features and basic roadmap

--- FDM_Fabric_Mod.py
+++ FDM_Fabric_Mod.py
@@ -25,6 +25,16 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>
 
+
+#Things to add
+#Error checking such as rafts and brims for zero layer printing
+#Functionality for multiple materials ie different material thicknesses
+#Functionality for improved material adhesion such as increased flow, print speed, etc
+#Test that head lift always occurs after the auto bed leveling
+
+
+
+
 import os.path
 
 
@@ -41,14 +51,44 @@ print('Welcome to the FDM/FFF gcode modification unit for fabric (GMUFF)','\n')
 print('Please make sure this .py file is in the same directory/folder as the GCode file(s).','\n','\n')
 print('Disclaimer:','\n','Use at your own and your machine\'s risk. ', '\n', 'Modify and test this code first, ensure it works for your setup.', '\n','See the readme for more information on what/how to test, it will be updated as new information is available.','\n', 'Please report bugs, make suggestions, and branch any custom versions to help others. ')
 
-#File location
+#File name
 
 inFile = input('Filename: ')
+			##printer specifications
+			##eventually change to .cfg, .json, or other input file format
+#printer max height
+machHeight = input('Printer max height (mm): ')
 
-#DebugTest
-print(inFile)
+#lift height
+liftHeight = input('Minimum height to left print head (mm): ')
+
+#wait time
+pauseTime = input('Hold time after lift (s): ')
+
+#interface layers
+totalNumFabrics = input('How many pieces to insert: ')
+
+#layer height to skip
+fabricThick = input('How thick is the material (mm): ')
+	#test valid number (<0.5)
+
+#interface layers begin
+begLayerNum = input('How many layers to skip before adding material: ')
+	#test number is int
+		#if not report error and request new number
+		#if zero notify that gcode will be offset to print on fabric
+#Test file for correct number of base layers
+	#if number of interface layers is greater than number of base layer print warning
+
+
+
+
+print(inFile) #DebugTest
 print (os.path.isfile(inFile))
 
 readInFile = open(inFile,'r')
 
+#Create copy file 
+#Test line and copy
+	#If test layer is a begLayerNum input lift and pause
 
