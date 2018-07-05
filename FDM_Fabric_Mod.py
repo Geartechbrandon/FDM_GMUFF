59db66c1bb0a77a77f77bd560f2f741351f648ac
Brandon Langdon <bfglangdon@gmail.com>
2018-07-04 22:21:45 -0700

    Added error checking
    Added loops for every variable to check if it is valid or not.

--- FDM_Fabric_Mod.py
+++ FDM_Fabric_Mod.py
@@ -59,52 +59,136 @@ inFile = input('Filename: ')
 			##printer specifications
 			##eventually change to .cfg, .json, or other input file format
 #printer max height
-machHeight = input('Printer max height (mm): ')
+heightTest = True                                           #Begin test for valid number and height warning if the printer is small
+while (heightTest == True):
+    try:
+        machHeight = float(input('Printer max height (mm): '))
+    except:
+        #Extra characters or anyhting that makes the variable become a non-integer gets caught
+        print ("That doesn't seem to be a valid number.")
+    else:
+        if (machHeight <= 10):
+            print ("Typo? A machine height of only ", machHeight," doesn't give adequate room for the head to lift with this program.")
+        else:
+            #Not much room to move the head so the fabric can be burned, head shifted, or injury can occur
+            if (machHeight < 100):
+                print ("A machine with height of only ", machHeight," is pretty small for this process. Please be careful to avoid burns.")
+            #Regardless the height, if the number is real, we exit
+            heightTest = False
 
-#lift height
-liftHeight = input('Minimum height to left print head (mm): ')
-
-#wait time
-pauseTime = input('Hold time after lift (min): ')
+fabricThickTest = True                                      #Begin test for valid number and thickness. Prints warning relevant to currenet development features
+while (fabricThickTest == True):
+    try:      
+        #layer height to skip
+        fabricThick = float(input('How thick is the material (mm): '))
+    except:
+        #Extra characters or anyhting that makes the variable become a non-integer gets caught
+        print ("That doesn't seem to be a valid number.")
+    else:
+        # Checks for negatives and zero thickness
+        if (fabricThick <= 0):
+            print ("Material must have a thickness greater than zero")
+            fabricThickTest = True
+        else:
+            #Currently there is no extrusion multiplier for the layer jump so it's risky to skip more than 1.5 the layer thickness as there will be little to no layer to layer bonding
+            if (fabricThick > 0.5):
+                print ("Fabric layer greater than 0.5 can cause problems with layer adhesion at this time.","\nGood guide rule is to keep the fabric layer less than 1.5 times the layer thickness.")
+                fabricThickTest = False
+            else:
+                #Everything looks good so far
+                fabricThickTest = False
+    #test valid number (<0.5)
 
 #interface layers
-totalNumFabrics = input('How many pieces to insert: ')
+totalNumFabricTest = True                                   #Begin value test
+while (totalNumFabricTest == True):
+    try:
+        totalNumFabrics = int(input('How many pieces to insert: '))
+    except:
+        #Extra characters or anyhting that makes the variable become a non-integer gets caught
+        print ("That doesn't seem to be a valid number.")
+    else:
+        #Should have at least one space to make, otherwise nothing happens
+        if (totalNumFabrics <= 0):
+            print ("Should have at least one piece to insert or gap to make.")
+            totalNumFabricTest = True  
+        #Everything looks good so far
+        else:
+            totalNumFabricTest = False 
 
-#layer height to skip
-fabricThick = input('How thick is the material (mm): ')
-	#test valid number (<0.5)
 
-#interface layers begin
-begLayerNum = input('How many layers to skip before adding material (starts at 0): ')
-	#test number is int
-		#if not report error and request new number
-		#if zero notify that gcode will be offset to print on fabric
+#where interface layers begin
+begLayerNumTest = True
+while (begLayerNumTest == True):
+    try:
+        begLayerNum = int(input('What layer do we start printing on (CURA starts at 0): '))
+    except:
+        #Extra characters or anyhting that makes the variable become a non-integer gets caught
+        print ("That doesn't seem to be a valid number.") 
+    else:
+        if (begLayerNum < 0):
+            print ("For CURA we need to start with at least LAYER:0.")
+            begLayerNumTest = True
+        else:
+            begLayerNumTest = False  
+
+
 #Test file for correct number of base layers
-	#if number of interface layers is greater than number of base layer print warning
+    #if number of interface layers is greater than number of base layer print warning
 
+liftHeightTest = True
+while (liftHeightTest == True):
+    #lift height
+    try:
+        liftHeight = float(input('Minimum height to lift printer head (mm): '))
+    except:
+        #Extra characters or anyhting that makes the variable become a non-integer gets caught
+        print ("That doesn't seem to be a valid number.")
+    else:
+        if (liftHeight >= machHeight):
+            print ("The machine height is ", machHeight," so the lifting height must be less than this.")
+            liftHeightTest = True
+        else:
+            if (liftHeight <= fabricThick) and (begLayerNum == 0):
+                print ("Need to light the head at least higher than the fabric layer.")
+                liftHeightTest = True
+            else:
+                liftHeightTest = False
 
+pauseTimeTest = True
+while (pauseTimeTest == True):
+    #wait time
+    try:
+        pauseTime = float(input('Hold time after lift (min): '))
+    except:
+        #Extra characters or anyhting that makes the variable become a non-integer gets caught
+        print ("That doesn't seem to be a valid number.")
+    else:
+        if (pauseTime < 0):
+            print ("Time must be at least zero. Ex: 0.2, 1, 1.4, etc.")
+        else:
+            pauseTimeTest = False
 
 #DebugTests
-print(inFile) 
-print(machHeight)
-print(liftHeight)
-print(pauseTime)
-print(totalNumFabrics)
-print(fabricThick)
-print(begLayerNum)
-
-print (os.path.isfile(inFile))
+#print(inFile) 
+#print(machHeight)
+#print(liftHeight)
+#print(pauseTime)
+#print(totalNumFabrics)
+#print(fabricThick)
+#print(begLayerNum)
+#print (os.path.isfile(inFile))
                                                     #
-    if inFile.endswith('.gcode'):                           #checks for .gcode
+if inFile.endswith('.gcode'):                           #checks for .gcode
         outfile = inFile[:-6] + '_ClothMod.gcode'
         print (outfile)
-    elif inFile.endswith('.txt'):                           #check for .txt file. Just at test at this time
+elif inFile.endswith('.txt'):                           #check for .txt file. Just at test at this time
         outfile = inFile[:-4] + '_ClothMod.gcode'
         print (outfile)
-    elif inFile.endswith('.g'):                             #check for .g file. Just a test at this time unless it has CURA format
+elif inFile.endswith('.g'):                             #check for .g file. Just a test at this time unless it has CURA format
         outfile = inFile[:-2] + '_ClothMod.gcode'
         print (outfile)
-    else:                                                   #catch all other error. Should loop back to file input as a test
+else:                                                   #catch all other error. Should loop back to file input as a test
         print ("I don't recognize this file type")
 
 writeOutFile = open(outfile, 'wt')                      #opens the copy file for writing into
@@ -118,8 +202,10 @@ writeOutFile = open(outfile, 'wt')                      #opens the copy file for
                                                             ##Probably would require a small flow rate adjustment for some machines
 zSearch ='Z'
 zPosNum = 0
-firstLayer = ';LAYER:0'
-changeBegin = false
+firstLayer = ';LAYER:' + str(begLayerNum)
+
+print ("First layer starts on: ",firstLayer)
+changeBegin = False
         #CURA converts the input mm/s into mm/min for Marlin
         #Time of 5 minutes hold is then 5 min to travel 10mm
         #10mm/5min = 2mm/min
@@ -127,10 +213,9 @@ liftHeightShort = liftHeight - 10
 holdTime = 10 / pauseTime    #mm/min
 
         #this will get written, line by line, to the file once the firt layer has been found
-skipParameterList = ["LIFT HEIGHT", 'G0 F3600 Z' + str(liftHeightShort), ";HOLD TIME", 'G0 F' + holdTime + ' Z' +liftHeight, 'RESUME PRINTING']
-
-print(';LAYER:' + str(counter))
-print (type(counter))
+skipParameterList = ["LIFT HEIGHT", 'G0 F3600 Z' + str(liftHeightShort), ";HOLD TIME", 'G0 F' + str(holdTime) + ' Z' + str(liftHeight), 'RESUME PRINTING']
+#print(';LAYER:' + str(counter))
+#print (type(counter))
 
 counter = 0
 
@@ -144,28 +229,25 @@ with open (inFile, 'rt') as readInFile:                 #with opens the file and
         lineLength = len(line)                          #reads the length of the current string variable for the current line
 
     #Find zero layer ;LAYER:0
-        if (begLayerNum == 0):
+        if (begLayerNum == 0) and (line == firstLayer):
             print ("Printing on fabric.")
-            changeBegin = true
-            #Insert skipParameterList            
+            changeBegin = true           
         elif (line == firstLayer):
             print ("Fabric layer found.")
             changeBegin = true
-            #Insert skipParameterList                   Should write all this to the file (doesn't need to know what the z layer was/should be yet) then get z layer info on next loop
 
-         #ends beginning layer search. find() outputs either -1 if not found or the index of the character(s) in the searched string
         zLineGrab = line.find(zSearch)                
     #write non changed lines to file
-        if (changeBegin == true and zLineGrab != (-1)):
+        if (changeBegin == True) and (zLineGrab != (-1)):
             #break line into movement and zNumber
             zLineGrab = zLineGrab + 1
             stringLength = lineLength - zLineGrab
             zStartPos = line[zLineGrab:]
             zStartPos = int(zStartPos)
-            zEndPosition = zStartPos + fabrickThick
+            zEndPosition = zStartPos + fabricThick
 
             #Add the fabric thickness to the zheight
-        else
+        else:
             writeOutFile.write(line)
     #modify 
 
@@ -182,25 +264,25 @@ with open (inFile, 'rt') as readInFile:                 #with opens the file and
    
         
         
-        elif (counter == begLayerNum):       #tests that both cases are true
-            skipBegin = true
-            origLine = line
-            truncateLine = line[zPosNum:]
+#        elif (counter == begLayerNum):       #tests that both cases are true
+#            skipBegin = true
+#            origLine = line
+#            truncateLine = line[zPosNum:]
              
-            editLine = truncateLine + 
+#            editLine = truncateLine + 
             
-        elif (line == layerNum):
-            ++counter
+#        elif (line == layerNum):
+#            ++counter
             
             
-            writeOutFile.write(line)
+#            writeOutFile.write(line)
                        
          
 
-    else:                                           #writes line to file
-            writeOutFile.write(line)
+#    else:                                           #writes line to file
+#            writeOutFile.write(line)
             
-    ++counter
+#    ++counter
 
         
     
